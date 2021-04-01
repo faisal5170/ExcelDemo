360b629ff552c140f0dbef27c57a2966cfb22e92
Faisal Pathan <faisalmpathan@gmail.com>
2021-04-01 16:11:39 +0530

    Initial commit

---
+++ ExcelDemo/ExcelDemo/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ExcelDemo
+{
+    public partial class Form1 : Form
+    {
+        System.Data.DataTable allRecords;
+        public Form1()
+        {
+            InitializeComponent();
+            insertExcel.Visible = false;
+        }
+
+        private void btnReadExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XLWorkbook wb = new XLWorkbook();
+                System.Data.DataTable dt = Read("select * from [DemoDB].[dbo].[Marksheets]");
+                wb.Worksheets.Add(dt, "ExcelFiles");
+                string folderPath = "D:\\Excel Files\\";
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                string path = Path.Combine("D:\\Excel Files\\", $"{DateTime.Now.Ticks.ToString("X")}_Excel.xlsx");
+                wb.SaveAs(path);
+                Display($"File is downloaded at following path: {path}");
+            }
+            catch (Exception exception)
+            {
+                Display(exception.Message.ToString());
+            }
+        }
+
+        private void openExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fname = "";
+                OpenFileDialog fdlg = new OpenFileDialog();
+                fdlg.Title = "Excel File Dialog";
+                fdlg.InitialDirectory = @"d:\";
+                fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+                fdlg.FilterIndex = 2;
+                fdlg.RestoreDirectory = true;
+                if (fdlg.ShowDialog() == DialogResult.OK)
+                {
+                    fname = fdlg.FileName;
+                }
+                allRecords = new System.Data.DataTable();
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                using (XLWorkbook workBook = new XLWorkbook(fname))
+                {
+                    IXLWorksheet workSheet = workBook.Worksheet(1);
+                    bool firstRow = true;
+                    foreach (IXLRow row in workSheet.Rows())
+                    {
+                        if (firstRow)
+                        {
+                            foreach (IXLCell cell in row.Cells())
+                            {
+                                allRecords.Columns.Add(cell.Value.ToString());
+                            }
+                            firstRow = false;
+                        }
+                        else
+                        {
+                            allRecords.Rows.Add();
+                            int i = 0;
+                            foreach (IXLCell cell in row.Cells())
+                            {
+                                allRecords.Rows[allRecords.Rows.Count - 1][i] = cell.Value.ToString();
+                                i++;
+                            }
+                        }
+                        dataGridView1.DataSource = allRecords;
+                        insertExcel.Visible = true;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                insertExcel.Visible = false;
+                allRecords = null;
+                Display(exception.Message.ToString());
+            }
+        }
+
+        public void Display(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        public System.Data.DataTable Read(string query)
+        {
+            System.Data.DataTable dataTable = new System.Data.DataTable();
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dataTable);
+            conn.Close();
+            da.Dispose();
+            return dataTable;
+        }
+
+        private void clearExcel_Click(object sender, EventArgs e)
+        {
+            allRecords = null;
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            insertExcel.Visible = false;
+            Display("Records cleared Successfully");
+        }
+
+        private void insertExcel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlBulkCopy bulk = new SqlBulkCopy(connection))
+                    {
+                        bulk.DestinationTableName = "[DemoDB].[dbo].[Marksheets]";
+                        bulk.ColumnMappings.Add(0, 1);
+                        bulk.ColumnMappings.Add(1, 2);
+                        bulk.ColumnMappings.Add(2, 3);
+                        bulk.ColumnMappings.Add(3, 4);
+                        bulk.WriteToServer(allRecords);
+                        Display("Data inserted successfully");
+                        dataGridView1.DataSource = null;
+                        dataGridView1.Rows.Clear();
+                        allRecords = null;
+                        insertExcel.Visible = false;
+                    }
+                }
+            }
+            catch (Exception excep)
+            {
+                insertExcel.Visible = false;
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                allRecords = null;
+                Display(excep.Message.ToString());
+            }
+        }
+    }
+}
---
+++ ExcelDemo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ExcelDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadExcel = new System.Windows.Forms.Button();
+            this.openExcel = new System.Windows.Forms.Button();
+            this.insertExcel = new System.Windows.Forms.Button();
+            this.clearExcel = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadExcel
+            // 
+            this.btnReadExcel.Location = new System.Drawing.Point(130, 35);
+            this.btnReadExcel.Name = "btnReadExcel";
+            this.btnReadExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnReadExcel.TabIndex = 0;
+            this.btnReadExcel.Text = "Read Excel";
+            this.btnReadExcel.UseVisualStyleBackColor = true;
+            this.btnReadExcel.Click += new System.EventHandler(this.btnReadExcel_Click);
+            // 
+            // openExcel
+            // 
+            this.openExcel.Location = new System.Drawing.Point(270, 34);
+            this.openExcel.Name = "openExcel";
+            this.openExcel.Size = new System.Drawing.Size(99, 23);
+            this.openExcel.TabIndex = 1;
+            this.openExcel.Text = "Choose Excel";
+            this.openExcel.UseVisualStyleBackColor = true;
+            this.openExcel.Click += new System.EventHandler(this.openExcel_Click);
+            // 
+            // insertExcel
+            // 
+            this.insertExcel.Location = new System.Drawing.Point(583, 35);
+            this.insertExcel.Name = "insertExcel";
+            this.insertExcel.Size = new System.Drawing.Size(135, 23);
+            this.insertExcel.TabIndex = 2;
+            this.insertExcel.Text = "Confirm Insert Data";
+            this.insertExcel.UseVisualStyleBackColor = true;
+            this.insertExcel.Click += new System.EventHandler(this.insertExcel_Click);
+            // 
+            // clearExcel
+            // 
+            this.clearExcel.Location = new System.Drawing.Point(399, 34);
+            this.clearExcel.Name = "clearExcel";
+            this.clearExcel.Size = new System.Drawing.Size(145, 23);
+            this.clearExcel.TabIndex = 3;
+            this.clearExcel.Text = "Clear Current Data";
+            this.clearExcel.UseVisualStyleBackColor = true;
+            this.clearExcel.Click += new System.EventHandler(this.clearExcel_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 85);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(775, 353);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.clearExcel);
+            this.Controls.Add(this.insertExcel);
+            this.Controls.Add(this.openExcel);
+            this.Controls.Add(this.btnReadExcel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadExcel;
+        private System.Windows.Forms.Button openExcel;
+        private System.Windows.Forms.Button insertExcel;
+        private System.Windows.Forms.Button clearExcel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
